eb5b3e4789d399d302389397e3df7e7219a918e3
mcDJIL <mdjauharil29@gmail.com>
2026-05-18 04:12:34 +0700

    refactor: add dashboard layout styles and integrate CSS into header

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
   integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+<link rel="stylesheet" href="../../assets/css/admin/dashboard.css">
 <link rel="stylesheet" href="../../assets/css/dark-mode-dashboard.css">
 <script>
   (function () {
